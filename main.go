f2ebcaf3265dacc9aca8a355befef9023a595f50
gdiet <gdiet@users.noreply.github.com>
2025-07-21 08:56:54 +0200

    validate api input

--- main.go
+++ main.go
@@ -6,9 +6,15 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
 
+var validate *validator.Validate
+
 func main() {
+	validate = validator.New(validator.WithRequiredStructEnabled())
+
 	http.HandleFunc("/api/games", handleGamesRequests)
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -27,6 +33,10 @@ func handleGamesRequests(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid JSON: " + err.Error(), http.StatusBadRequest)
 		return
 	}
+	if err := validate.Struct(body); err != nil {
+		http.Error(w, "Validation failed: " + err.Error(), http.StatusBadRequest)
+		return
+	}
 
 	gameID := fmt.Sprintf("%06d", rand.Intn(1000000))
 
@@ -37,8 +47,8 @@ func handleGamesRequests(w http.ResponseWriter, r *http.Request) {
 }
 
 type PostGamesBody struct {
-	BoardLayout            string `json:"boardLayout"`
-	FreezeTimeMilliseconds int    `json:"freezeTimeMilliseconds"`
+	BoardLayout            string `json:"boardLayout" validate:"required,oneof=standard large"`
+	FreezeTimeMilliseconds int    `json:"freezeTimeMilliseconds" validate:"required,gte=0,lte=120000"`
 }
 
 type GameCommand string
