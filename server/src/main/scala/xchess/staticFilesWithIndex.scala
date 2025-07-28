c643d8377723a209358e587829a40345a7816df6
gdiet <gdiet@users.noreply.github.com>
2025-07-28 21:29:40 +0200

    cleanup of staticFilesWithIndex

--- server/src/main/scala/xchess/staticFilesWithIndex.scala
+++ server/src/main/scala/xchess/staticFilesWithIndex.scala
@@ -2,25 +2,30 @@ package xchess
 
 import cask.endpoints.{QueryParamReader, StaticUtil}
 import cask.model.Request
-import cask.router.HttpEndpoint
+import cask.model.Response.Raw
+import cask.router.{HttpEndpoint, Result}
 
-class staticFilesWithIndex(val path: String, headers: Seq[(String, String)] = Nil) extends HttpEndpoint[String, Seq[String]] {
-  val methods = Seq("get")
-  type InputParser[T] = QueryParamReader[T]
+type Endpoint = HttpEndpoint[String, Seq[String]]
 
-  override def subpath = true
+/** Adapted version of [[cask.endpoints.staticFiles]] annotation that serves index.html for directories. */
+class staticFilesWithIndex(val path: String, headers: Seq[(String, String)] = Nil) extends Endpoint:
+  override val methods: Seq[String]                    = Seq("get")
+  override def subpath                                 = true
+  override def wrapPathSegment(s: String): Seq[String] = Seq(s)
+  override type InputParser[T]                         = QueryParamReader[T]
 
-  def wrapFunction(ctx: Request, delegate: Delegate) = {
+  override def wrapFunction(ctx: Request, delegate: Delegate): Result[Raw] =
     delegate(ctx, Map()).map { t =>
-      val (path, contentTypeOpt) = StaticUtil.makePathAndContentType(t, ctx)
-      println(s"${ctx.exchange.getRequestPath} -> $path")
-      if java.nio.file.Files.isDirectory(java.nio.file.Paths.get(path)) then {
-        val contentType = java.nio.file.Files.probeContentType(java.nio.file.Paths.get(path + "/index.html"))
-        cask.model.StaticFile(path + "/index.html", headers :+ ("Content-Type" -> contentType))
-      } else
-        cask.model.StaticFile(path, headers ++ contentTypeOpt.map("Content-Type" -> _))
-    }
-  }
+      val leadingSlash = if (t.startsWith("/")) "/" else ""
+      val path = leadingSlash + (cask.internal.Util.splitPath(t) ++ ctx.remainingPathSegments.flatMap(cask.internal.Util.splitPath))
+        .filter(s => s != "." && s != "..")
+        .mkString("/")
+      val nioPath = java.nio.file.Paths.get(path)
 
-  def wrapPathSegment(s: String): Seq[String] = Seq(s)
-}
+      if java.nio.file.Files.isDirectory(nioPath) then
+        val contentType = java.nio.file.Files.probeContentType(nioPath.resolve("/index.html"))
+        cask.model.StaticFile(path + "/index.html", headers :+ ("Content-Type" -> contentType))
+      else
+        val contentType = java.nio.file.Files.probeContentType(nioPath)
+        cask.model.StaticFile(path, headers :+ ("Content-Type" -> contentType))
+    }
