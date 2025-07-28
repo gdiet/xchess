64ff532fe41229186d7aa49e1e8f292310688650
gdiet <gdiet@users.noreply.github.com>
2025-07-28 22:53:06 +0200

    possible solution, can be improved

--- server/src/main/scala/xchess/Server.scala
+++ server/src/main/scala/xchess/Server.scala
@@ -22,20 +22,36 @@ object Server extends cask.MainRoutes:
 
   // 3. Catch-all Route für alle anderen Routen außer /api
   @cask.get("/:path", subpath = true)
-  def fallback(ctx: cask.Request, path: Seq[String]) = {
-    val fullPath = "/" + path.mkString("/")
-    if (fullPath.startsWith("/api") || fullPath.startsWith("/assets")) {
-      cask.Response("Not Found", statusCode = 404)
-    } else {
-      val indexPath = Paths.get("src/main/resources/index.html")
-      if (Files.exists(indexPath)) {
-        val content = Files.readString(indexPath)
-        cask.Response(content, headers = List("Content-Type" -> "text/html"))
-      } else {
-        cask.Response("index.html not found", statusCode = 500)
-      }
-    }
-  }
+  def fallback(ctx: cask.Request, path: Seq[String]) =
+    val pathString = path.mkString("src/main/resources/", "/", "")
+    val nioPath = java.nio.file.Paths.get(pathString)
+    if java.nio.file.Files.isDirectory(nioPath) then
+      val contentType = java.nio.file.Files.probeContentType(nioPath.resolve("/index.html"))
+      cask.model.StaticFile(pathString + "/index.html", Seq("Content-Type" -> contentType))
+    else
+      val contentType = java.nio.file.Files.probeContentType(nioPath)
+      cask.model.StaticFile(pathString, Seq("Content-Type" -> contentType))
+
+  @cask.get("")
+  def root() =
+    val contentType = java.nio.file.Files.probeContentType(java.nio.file.Paths.get("src/main/resources/index.html"))
+    cask.model.StaticFile("src/main/resources/index.html", Seq("Content-Type" -> contentType))
+
+
+//
+//
+//    val fullPath = "/" + path.mkString("/")
+//    if (fullPath.startsWith("/api") || fullPath.startsWith("/assets")) {
+//      cask.Response("Not Found", statusCode = 404)
+//    } else {
+//      val indexPath = Paths.get("src/main/resources/index.html")
+//      if (Files.exists(indexPath)) {
+//        val content = Files.readString(indexPath)
+//        cask.Response(content, headers = List("Content-Type" -> "text/html"))
+//      } else {
+//        cask.Response("index.html not found", statusCode = 500)
+//      }
+//    }
 
   initialize()
   println(s"xChess server started at port $port")
