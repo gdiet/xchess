01a59c09203a7deb21708a4b7cc4ec5d66b81dcc
gdiet <gdiet@users.noreply.github.com>
2025-07-30 22:38:17 +0200

    get nice types

--- server/src/main/scala/xchess/game/Board.scala
+++ server/src/main/scala/xchess/game/Board.scala
@@ -1,61 +1,67 @@
 package xchess.game
 
-/* K - King
- * Q - Queen
- * B - Bishop
- * N - Knight
- * R - Rook
- * P - Pawn, not yet moved
- * M - Pawn, already moved
- * 
- * Uppercase white, lowercase black */
+import scala.collection.immutable.ListMap
 
-type Plan = (from: Square, to: Square)
-case class Game(board: Board, plannedMoves: Seq[Plan]) {
-}
+def firstScheduledMove(board: Board, plannedMoves: ListMap[Square, Square], gameTime: Long): Option[(Square, Square)] =
+  plannedMoves.collectFirst { case (from, to) if board.map.get(from).exists(_.frozenUntil <= gameTime) => (from, to) }
 
-type Rank   = Int
-type File   = Int
-type Square = (file: File, rank: Rank)
-type Piece  = Char
-case class MapEntry(piece: Piece, frozenUntil: Long)
+def tryMove(board: Board, from: Square, to: Square): Object =
+  if (from == to)
+    ???
+  else board.map.get(from) match {
+    case None => ???
+    case Some((piece, _)) =>
+      piece.value.toUpper match {
+        case 'K' =>
+          if (Math.abs(from.file - to.file) > 1 || Math.abs(from.rank - to.rank) > 1)
+            ???
+          else
+            throw new IllegalArgumentException(s"Invalid king move from $from to $to")
+          // King move logic
+        case 'Q' => ??? // Queen move logic
+        case 'B' => ??? // Bishop move logic
+        case 'N' => ??? // Knight move logic
+        case 'R' => ??? // Rook move logic
+        case 'P' => ??? // Pawn not moved logic
+        case 'M' => ??? // Pawn already moved logic
+        case _   => throw new IllegalArgumentException(s"Unknown piece type: $piece")
+      }
+  }
+
 case class Board(size: Square, map: Map[Square, MapEntry])
 
-object Board {
-  def apply(name: String, initialFreezeTime: Long = 3000): Board = {
-    val lines = layout (name).linesIterator.toSeq
-    val size = (lines.head.length, lines.length)
-    val pieces = for {
-      (line, y) <- lines.zipWithIndex
-      (piece, x) <- line.zipWithIndex
-      if piece != '+'
-    } yield (x, y) -> MapEntry (piece, initialFreezeTime)
-    new Board (size, pieces.toMap)
-  }
+def createBoard(name: String, initialFreezeTime: Long = 3000): Board = {
+  val lines = boardLayout(name).linesIterator.toSeq
+  val size = (file = File(lines.head.length), rank = Rank(lines.length))
+  val pieces = for {
+    (line,  y) <- lines.zipWithIndex
+    (piece, x) <- line .zipWithIndex
+    if piece != '+'
+  } yield (File(x), Rank(y)) -> (Piece(piece), initialFreezeTime)
+  Board (size, pieces.toMap)
+}
 
-  private def layout(name: String): String = name match {
-    case "large" => """|RRNNBBQKQBBNNRR
-                       |RRNNBBQQQBBNNRR
-                       |PPPPPPPPPPPPPPP
-                       |PPPPPPPPPPPPPPP
-                       |+++++++++++++++
-                       |+++++++++++++++
-                       |+++++++++++++++
-                       |+++++++++++++++
-                       |+++++++++++++++
-                       |ppppppppppppppp
-                       |ppppppppppppppp
-                       |rrnnbbqqqbbnnrr
-                       |rrnnbbqkqbbnnrr""".stripMargin
+private def boardLayout(name: String): String = name match {
+  case "large" => """|RRNNBBQKQBBNNRR
+                     |RRNNBBQQQBBNNRR
+                     |PPPPPPPPPPPPPPP
+                     |PPPPPPPPPPPPPPP
+                     |+++++++++++++++
+                     |+++++++++++++++
+                     |+++++++++++++++
+                     |+++++++++++++++
+                     |+++++++++++++++
+                     |ppppppppppppppp
+                     |ppppppppppppppp
+                     |rrnnbbqqqbbnnrr
+                     |rrnnbbqkqbbnnrr""".stripMargin
 
-    case _ =>       """|RNBQKBNR
-                       |PPPPPPPP
-                       |++++++++
-                       |++++++++
-                       |++++++++
-                       |++++++++
-                       |pppppppp
-                       |rnbqkbnr""".stripMargin
-  }
-
+  case _ =>       """|RNBQKBNR
+                     |PPPPPPPP
+                     |++++++++
+                     |++++++++
+                     |++++++++
+                     |++++++++
+                     |pppppppp
+                     |rnbqkbnr""".stripMargin
 }
---
+++ server/src/main/scala/xchess/game/types.scala
@@ -0,0 +1,31 @@
+package xchess.game
+
+/** K - King
+ * Q - Queen
+ * B - Bishop
+ * N - Knight
+ * R - Rook
+ * P - Pawn, not yet moved
+ * M - Pawn, already moved
+ *
+ * Uppercase white, lowercase black */
+opaque type Piece = Char
+object Piece:
+  def apply(value: Char): Piece = value
+  extension (r: Piece) def value: Char = r
+  extension (r: Piece) def isWhite: Boolean = r.isUpper
+
+opaque type Rank = Int // 1 to 8 in standard chess, here 0 to 7 (more for large boards)
+object Rank:
+  def apply(value: Int): Rank = value
+  extension (r: Rank) def value: Int = r
+  extension (r: Rank) def - (other: Rank): Int = r.value - other.value
+
+opaque type File = Int // a to h in standard chess, here 0 to 7 (more for large boards)
+object File:
+  def apply(value: Int): File = value
+  extension (r: File) def value: Int = r
+  extension (r: File) def - (other: File): Int = r.value - other.value
+
+type Square = (file: File, rank: Rank)
+type MapEntry = (piece: Piece, frozenUntil: Long)
