7496e32c59faedf617559aa0060504bbe1a9e618
gdiet <gdiet@users.noreply.github.com>
2025-08-01 19:27:56 +0200

    split up large function

--- server/src/main/scala/xchess/game/Game.scala
+++ server/src/main/scala/xchess/game/Game.scala
@@ -10,22 +10,19 @@ case class Game(
 
 @annotation.tailrec
 def executeAllScheduledMoves(game: Game, gameTime: GameTime): Game =
-  executeNextScheduledMove(game, gameTime) match
+  executeNextScheduledMove(game, game.plannedMoves.iterator, gameTime) match
     case None => game // no more moves to execute
     case Some(newGame) => executeAllScheduledMoves(newGame, gameTime) // recursively execute next move
 
-def executeNextScheduledMove(game: Game, gameTime: GameTime): Option[Game] =
+@annotation.tailrec
+def executeNextScheduledMove(game: Game, moves: Iterator[(Square, Square)], gameTime: GameTime): Option[Game] =
   import game.*
-  @annotation.tailrec
-  def recurse(moves: Iterator[(Square, Square)]): Option[Game] = {
-    moves.nextOption match
-      case None => None // no more planned moves
-      case Some(from, to) => board.map.get(from) match
-        case None => Some(game.copy(plannedMoves = plannedMoves - from)) // piece not found, remove move
-        case Some(_, frozenUntil) if frozenUntil > gameTime => recurse(moves) // piece is frozen, skip this move
-        case Some((piece, _)) => Some(handleMoveCommand(game, piece, from, to, gameTime))
-  }
-  recurse(game.plannedMoves.iterator)
+  moves.nextOption match
+    case None => None // no more planned moves
+    case Some(from, to) => board.map.get(from) match
+      case None => Some(game.copy(plannedMoves = plannedMoves - from)) // piece not found, remove move
+      case Some(_, frozenUntil) if frozenUntil > gameTime => executeNextScheduledMove(game, moves, gameTime) // piece is frozen, skip this move
+      case Some((piece, _)) => Some(handleMoveCommand(game, piece, from, to, gameTime))
 
 def handleMoveCommand(game: Game, piece: Piece, from: Square, to: Square, gameTime: GameTime): Game =
   import game.*
