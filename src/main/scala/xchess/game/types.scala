280ccf4b32efba4a5f826d94442acc75499dbd8b
gdiet <gdiet@users.noreply.github.com>
2025-09-11 18:01:06 +0200

    cleanup

--- src/main/scala/xchess/game/types.scala
+++ src/main/scala/xchess/game/types.scala
@@ -25,26 +25,25 @@ opaque type Row = Int // rank 1 to 8 in standard chess, here 0 to 7 (more for la
 object Row:
   def apply(value: Int): Row = value
   extension (r: Row) def value: Int = r
-  extension (r: Row) def - (other: Row): Int = r.value - other.value
+  extension (r: Row) def - (other: Row): Int = r - other
 
 opaque type Col = Int // file a to h in standard chess, here 0 to 7 (more for large boards)
 object Col:
   def apply(value: Int): Col = value
-  extension (r: Col) def value: Int = r
-  extension (r: Col) def - (other: Col): Int = r.value - other.value
+  extension (c: Col) def value: Int = c
+  extension (c: Col) def - (other: Col): Int = c - other
 
 opaque type GameTime = Long // in arbitrary ticks, 0 is the start of the game
 object GameTime:
   def apply(value: Long): GameTime = value
   extension (r: GameTime) def value: Long = r
-  extension (r: GameTime) def > (other: GameTime): Boolean = r.value > other.value
-  extension (r: GameTime) def + (timespan: Long): GameTime = r.value + timespan
+  extension (r: GameTime) def > (other: GameTime): Boolean = r > other
+  extension (r: GameTime) def + (timespan: Long): GameTime = r + timespan
 
 type Square = (col: Col, row: Row)
 object Square:
   def apply(string: String): Square = (col = string.head - 'A', row = string.tail.toInt - 1)
-  def plus(s: Square)(cols: Int, rows: Int): Square = (s.col + cols, s.row + rows)
   extension (s: Square) def string: String = s"${('A' + s.col).toChar}${s.row + 1}"
-  extension (s: Square) def +(cols: Int, rows: Int): Square = (s.col + cols, s.row + rows)
+  extension (s: Square) def + (cols: Int, rows: Int): Square = (s.col + cols, s.row + rows)
 
 type PieceOnMap = (piece: Piece, frozenUntil: GameTime)
