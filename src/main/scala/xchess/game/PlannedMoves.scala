c869443130493283c980390db88671d0ed879366
gdiet <gdiet@users.noreply.github.com>
2025-09-14 10:55:18 +0200

    restructure plan execution to return more information

--- src/main/scala/xchess/game/PlannedMoves.scala
+++ src/main/scala/xchess/game/PlannedMoves.scala
@@ -18,17 +18,17 @@ case class PlannedMoves(
   private type PlanReference = (from: Square, forWhite: Boolean)
 
   /** @return planned moves, executed moves, removed plans. */
-  final def executePlans2(upToTime: GameTime, freezeTime: Long): (PlannedMoves, Seq[Move], Seq[PlanReference] ) =
+  final def executePlans(upToTime: GameTime, freezeTime: Long): (PlannedMoves, Seq[Move], Seq[PlanReference] ) =
     timePlan
       .takeWhile { case (time, _) => time <= upToTime }
       .foldLeft((this, Seq[Move](), Seq[PlanReference]())) {
         case ((currentPlannedMoves, executedMoves, removedPlans), (time, planIds)) =>
-          val (nextPlannedMoves, executed, removed) = executePlans2(time, planIds, freezeTime)
+          val (nextPlannedMoves, executed, removed) = executePlans(time, planIds, freezeTime)
           (nextPlannedMoves, executedMoves ++ executed, removedPlans ++ removed)
       }
 
-  private def executePlans2(time: GameTime, planIds: Seq[PlanId], freezeTime: Long):
-    (PlannedMoves, Seq[Move], Seq[PlanReference]) = {
+  private def executePlans(time: GameTime, planIds: Seq[PlanId], freezeTime: Long):
+        (PlannedMoves, Seq[Move], Seq[PlanReference]) =
     val whiteFirst = time.whiteFirst
     val (first, second) = planIds.flatMap(plans.get).partition(_.isWhite == whiteFirst)
     val plansToExecute = interleave(first, second)
@@ -40,61 +40,25 @@ case class PlannedMoves(
             case Some((newBoard, target)) => (newBoard, currentMoves :+ (from, target))
       }
     val squaresToRemovePlansFrom = moves.map(_.to) ++ plansToExecute.map(_.from)
-    // FIXME continue
-    ???
-  }
-
-  /** FIXME needs to return the list of executed moves and the list of removed plans. */
-  @annotation.tailrec
-  final def executePlans(upToTime: GameTime, freezeTime: Long): PlannedMoves =
-    timePlan.headOption match
-      case None => this
-      case Some((time, planIds)) =>
-        if time > upToTime then this
-        else executePlans(time, planIds, freezeTime).executePlans(upToTime, freezeTime)
-
-  private def executePlans(time: GameTime, planIds: Seq[PlanId], freezeTime: Long): PlannedMoves =
-    val whiteFirst = time.whiteFirst
-    val (first, second) = planIds.flatMap(plans.get).partition(_.isWhite == whiteFirst)
-    val plansToExecute = interleave(first, second)
-    val (newBoard, targets) =
-      plansToExecute.foldLeft((board, Seq[Square]())) {
-        case ((currentBoard, currentTargets), (_, from, to, _)) =>
-          currentBoard.executeMove(from, to, time + freezeTime) match
-            case None => (currentBoard, currentTargets)
-            case Some((newBoard, target)) => (newBoard, currentTargets :+ target)
-      }
-    val squaresToRemovePlansFrom = targets ++ plansToExecute.map(_.from)
-    // TODO should probably be inlined
-    squaresToRemovePlansFrom.flatMap(boardPlan.get) // TODO and so on
-    squaresToRemovePlansFrom.foldLeft(this)(_.removePlannedMove(_)).copy(board = newBoard)
-
-  private def removePlannedMove(from: Square): PlannedMoves =
-    boardPlan.get(from) match
-      case None => this
-      case Some(planId) =>
-        plans.get(planId) match
-          case None => assert(false); this
-          case Some(plan) =>
-            copy(
-              boardPlan = boardPlan - from,
-              timePlan = timePlan.updatedWith(plan.time) {
-                case None => assert(false); None
-                case some => some.map(_.filter(_ != planId))
-              },
-              plans = plans - planId
-            )
+    val plansToRemove = squaresToRemovePlansFrom.flatMap(boardPlan.get)
+    val removedPlansRefs = plansToRemove.flatMap { plans.get(_).map(plan => (plan.from, plan.isWhite)) }
+    (copy(
+      board = newBoard,
+      boardPlan = boardPlan -- squaresToRemovePlansFrom,
+      timePlan = timePlan - time,
+      plans = plans -- plansToRemove
+    ), moves, removedPlansRefs)
 
   /** Plans a move for a player if possible.
     *
-    * @return planned time + planned moves, or None for invalid plan. */
-  def plan(from: Square, to: Square, forWhite: Boolean, earliestTime: GameTime): Option[(GameTime, PlannedMoves)] =
+    * @return planned moves + planned time for the move, or None for invalid plan. */
+  def plan(from: Square, to: Square, forWhite: Boolean, earliestTime: GameTime): Option[(PlannedMoves, GameTime)] =
     assert(from != to)
     if !board.size.contains(to) then None
     else board.map.get(from).flatMap { case (piece, frozenUntil) =>
       if piece.isWhite != forWhite then None else
         val time = max(frozenUntil, earliestTime)
-        Some((time, addPlannedMove(piece, from, to, time)))
+        Some((addPlannedMove(piece, from, to, time), time))
     }
 
   private def addPlannedMove(piece: Piece, from: Square, to: Square, time: GameTime): PlannedMoves =
