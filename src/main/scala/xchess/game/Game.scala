09add4f235bbcbff4dd721e8fd78349b98a362b7
gdiet <gdiet@users.noreply.github.com>
2025-09-15 16:09:52 +0200

    some initial websocket messages

--- src/main/scala/xchess/game/Game.scala
+++ src/main/scala/xchess/game/Game.scala
@@ -1,11 +1,14 @@
 package xchess.game
 
+import Square.string
+
 class Game(id: String, options: GameOptions): // id only for logging purposes
   private var subscriptions: Set[Subscription] = Set()
   private var plannedMoves: PlannedMoves = PlannedMoves(Board(options))
 
   def subscribe(subscription: Subscription): Unit = synchronized {
     subscriptions += subscription
+    sendInitialMessages(subscription)
   }
 
   def unsubscribe(subscription: Subscription): Unit = synchronized {
@@ -13,16 +16,19 @@ class Game(id: String, options: GameOptions): // id only for logging purposes
   }
 
   def receiveMessage(message: String): Unit = synchronized {
-    ???
+    println(s"[$id] received message: $message")
+    // FIXME implement
   }
 
-  private def sendInitialMessages(subscription: Subscription): Unit = synchronized {
-    // boardsize
-    // millisPerTick
-    // freezeTicks
-    // clock
-    // add
+  private def sendInitialMessages(subscription: Subscription): Unit =
+    subscription.send(s"boardsize ${plannedMoves.board.size.string}")
+    subscription.send(s"millisPerTick ${options.millisPerTick}")
+    subscription.send(s"freezeTicks ${options.freezeTicks}")
+    // TODO clock: send current game time and clock status
+    plannedMoves.board.map.foreach((square, entry) =>
+      subscription.send(s"add ${square.string} ${entry.piece.value} ${entry.frozenUntil}")
+    )
+    // TODO when sending plans, we need to know whether the player is white or black
     // plan
     // chat
-    // connected
-  }
+    // connected (as status message)
