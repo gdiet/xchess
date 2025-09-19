96aa79dbbecfc2b94b9b28cf5a1921fb70a6dfa9
gdiet <gdiet@users.noreply.github.com>
2025-09-19 14:03:53 +0200

    load images

--- src/main/resources/xchess.js
+++ src/main/resources/xchess.js
@@ -1,12 +1,14 @@
 // @ts-check
-// For release, use the minified library pixi/pixi.min.mjs
-import { Application, Graphics } from './pixi/pixi.mjs'
 
-// Create the Pixi Application for the chess board
-const app = new Application()
-const htmlContainer = document.body
-await app.init({ background: '#1099bb', resizeTo: htmlContainer })
-htmlContainer.replaceChildren(app.canvas)
+import { Application, Assets, Graphics, Sprite } from './pixi/pixi.mjs' // For release, use pixi/pixi.min.mjs
+
+const gameId = new URLSearchParams(window.location.search).get('game') || 'test'
+const isWhite = new URLSearchParams(window.location.search).get('color') != 'black'
+console.log(`xChess game '${gameId}' as ${isWhite ? 'white' : 'black'}`)
+
+const pixi = new Application()
+await pixi.init({ background: '#1099bb', resizeTo: document.body })
+document.body.replaceChildren(pixi.canvas)
 
 const cols = 10
 const rows = 8
@@ -19,19 +21,38 @@ for (var x = 0; x < cols; x++)
 for (var y = x%2; y < rows; y += 2)
   chessBoard.rect(x, y, 1, 1)
 chessBoard.fill(0xa0a0a0)
-app.stage.addChild(chessBoard)
+pixi.stage.addChild(chessBoard)
+
+await loadImages()
+
+// FIXME demo code, remove soon
+// add board to container, resize container
+// possibly invert coordinats like: container.scale.y = -1
+const mySprite = new Sprite(Assets.get("P"));
+mySprite.setSize(1, 1);
+mySprite.x = 0
+mySprite.y = 7
+chessBoard.addChild(mySprite);
+
+async function loadImages() {
+  await Promise.all("BKNPQR".split("").map(async piece => {
+    await Assets.load({ alias: piece,               src: `cardinal/w${piece}.svg`, data: { resolution: 2 } });
+    await Assets.load({ alias: piece.toLowerCase(), src: `cardinal/b${piece}.svg`, data: { resolution: 2 } });
+  }));
+  console.log(`images loaded`)
+}
 
 /**
  * @param {Number} cols 
  * @param {Number} rows 
  */
 function resizeChessBoard(cols, rows) {
-  const xBound = app.canvas.width * 0.9
-  const yBound = app.canvas.height * 0.9
+  const xBound = pixi.canvas.width * 0.9
+  const yBound = pixi.canvas.height * 0.9
   const scale = Math.min(xBound / cols, yBound / rows)
-  chessBoard.x = (app.canvas.width / scale - cols) / 2
-  chessBoard.y = (app.canvas.height / scale - rows) / 2
-  app.stage.scale.set(scale)
+  chessBoard.x = (pixi.canvas.width / scale - cols) / 2
+  chessBoard.y = (pixi.canvas.height / scale - rows) / 2
+  pixi.stage.scale.set(scale)
 }
 
 resizeChessBoard(cols, rows)
--- src/main/scala/xchess/game/Game.scala
+++ src/main/scala/xchess/game/Game.scala
@@ -99,9 +99,8 @@ class Game(id: String, options: GameOptions, scheduler: ScheduledExecutorService
     subscriptions.foreach(subscription => if subscription.isWhite == forWhite then subscription.send(out))
 
   private def sendInitialMessages(subscription: Subscription): Unit =
-    send(subscription, s"clock ${clock.time} ${if clock.isStopped then "stopped" else "running"}")
-    send(subscription, s"player ${if subscription.isWhite then "white" else "black"}")
     send(subscription, s"boardsize ${plannedMoves.board.size.string}")
+    send(subscription, s"clock ${clock.time} ${if clock.isStopped then "stopped" else "running"}")
     send(subscription, s"millisPerTick ${options.millisPerTick}")
     send(subscription, s"freezeTicks ${options.freezeTicks}")
     plannedMoves.board.map.foreach((square, entry) =>
